6042d99e26c53614caadfbb05a0edec026ad21e5
Ahtesham Sarwar <ahteshamsarwar333@gmail.com>
2020-08-08 20:11:41 +0500

    HomeScreen designed and listView is populated with brands.

--- app/src/main/java/com/example/onlinestore/HomeScreen.kt
+++ app/src/main/java/com/example/onlinestore/HomeScreen.kt
@@ -2,11 +2,32 @@ package com.example.onlinestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_home_screen.*
 
 class HomeScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+        val fetchBrandUrl = "http://192.168.10.4/OnlineStore/fetch_brands.php"
+        var brandList = ArrayList<String>()
+        val requestQ: RequestQueue = Volley.newRequestQueue(this@HomeScreen)
+        val jAR = JsonArrayRequest(Request.Method.GET,fetchBrandUrl,null,Response.Listener { response ->
+            for (jObjectIndex in 0.until(response.length())){
+                brandList.add(response.getJSONObject(jObjectIndex).getString("brand"))
+            }
+            var adapter = ArrayAdapter(this@HomeScreen,R.layout.brand_listview_row,brandList)
+            brandListView.adapter = adapter
+        },Response.ErrorListener { error ->
+            Toast.makeText(this@HomeScreen,error.message,Toast.LENGTH_SHORT).show()
+        })
+        requestQ.add(jAR)
     }
 }

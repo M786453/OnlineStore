46d322aa3003924cd3b7b0a672df8c1e031bdbcf
Ahtesham Sarwar <ahteshamsarwar333@gmail.com>
2020-08-10 16:09:08 +0500

    Intent for transitioning to TemporaryPlaceOrder activity is added.

--- app/src/main/java/com/example/onlinestore/AmountFragment.kt
+++ app/src/main/java/com/example/onlinestore/AmountFragment.kt
@@ -1,11 +1,14 @@
 package com.example.onlinestore
 
 import android.app.DialogFragment
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
 
 class AmountFragment : DialogFragment() {
 
@@ -15,7 +18,12 @@ class AmountFragment : DialogFragment() {
     ): View? {
         // Inflate the layout for this fragment
         var fragmentView = inflater.inflate(R.layout.fragment_amount, container, false)
-
+        var edtAmountOfProduct = fragmentView.findViewById<EditText>(R.id.edtAmountOfProduct)
+        var btnAddToCart = fragmentView.findViewById<ImageButton>(R.id.addToCartImgBtn)
+        btnAddToCart.setOnClickListener {
+            var intent = Intent(fragmentView.context,TemporaryPlaceOrder::class.java)
+            startActivity(intent)
+        }
         return fragmentView
     }
 
